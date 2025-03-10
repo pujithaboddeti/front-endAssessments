70820c5247ecd27a3a8f7249140acd36b48cc3f0
pujithaboddeti <125138716+pujithaboddeti@users.noreply.github.com>
2025-03-10 12:15:07 +0530

    Add files via upload

---
+++ JSevents5.js
@@ -0,0 +1,17 @@
+// Creating a JSON object
+let user = {
+    name: "Pavani",
+    age: 25,
+    location: "Vishakhapatnam",
+    skills: {
+        programming: "JavaScript",
+        hobbies: ["reading", "traveling", "cooking"]
+    }
+};
+
+// Accessing properties using dot notation
+console.log("Name: ", user.name);
+console.log("Age: ", user.age);
+console.log("Location: ", user.location);
+console.log("Programming Skill: ", user.skills.programming);
+console.log("Hobbies: ", user.skills.hobbies);
